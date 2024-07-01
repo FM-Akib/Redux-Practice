789b91ff1b2436a94ce2260a45740970b379ee21
FM-Akib <muntasirakib08@gmail.com>
2024-07-01 11:59:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import Home from './Pages/Home'
+
+function App() {
+
+
+  return (
+    <>
+    
+     <div className="  ">
+      <h1 className="font-semibold text-gray-600 text-3xl text-center mt-10">Redux Practice</h1>
+      <Home/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
