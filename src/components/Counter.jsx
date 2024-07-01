bed822e027e77c2d2ad7d3f969af6b30bea3dcd6
FM-Akib <muntasirakib08@gmail.com>
2024-07-01 12:34:31 +0600

    updated

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,41 @@
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, decrementByAmount, increment, incrementByAmount } from "../redux/feature/counter/counterSlice";
 
 
 const Counter = () => {
+    const count = useSelector(state=> state.counter.value)
+    const dispatch = useDispatch()
     return (
-        <div className="bg-yellow-100 px-10 py-5 flex justify-center items-center mt-20">
-            <button className="btn bg-emerald-700 px-4 py-2 rounded-md text-white">
-                Increment
+        <div className="bg-yellow-100 px-10 py-5 flex flex-col gap-5 justify-center items-center mt-20">
+          
+          
+            <div className="">
+            <button className="btn bg-emerald-700  px-4 py-2 rounded-md text-white"
+            onClick={()=>dispatch(increment())}>
+               Increment
             </button>
-            <span className="px-6 py-2 font-semibold text-xl">0</span>
-            <button className="btn bg-red-700 px-4 py-2 rounded-md text-white">
+
+            <span className="px-6 py-2 font-semibold text-xl">{count}</span>
+
+            <button className="btn bg-red-700 px-4 py-2 rounded-md text-white"
+            onClick={()=>dispatch(decrement())}>
                 Decrement
             </button>
+            </div>
+
+
+            <div className="">
+            <button className="btn bg-fuchsia-700 mr-10 px-4 py-2 rounded-md text-white"
+            onClick={()=>dispatch(incrementByAmount(5))}>
+               Increment by 5 
+            </button>
+            
+            <button className="btn bg-red-700 px-4  py-2 rounded-md text-white"
+            onClick={()=>dispatch(decrementByAmount(5))}>
+                Decrement by 5
+            </button>
+            </div>
+
         </div>
     );
 };
--- src/redux/feature/counter/counterSlice.js
+++ src/redux/feature/counter/counterSlice.js
@@ -14,11 +14,14 @@ export const counterSlice = createSlice({
     },
     incrementByAmount: (state, action) => {
       state.value += action.payload
+    },
+    decrementByAmount: (state,action)=>{
+      state.value = state.value-action.payload
     }
   }
 })
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const { increment, decrement, incrementByAmount,decrementByAmount } = counterSlice.actions
 
 export default counterSlice.reducer
